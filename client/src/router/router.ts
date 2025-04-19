071bdf0c86ce6883b41ec6f49e7e91ce6b71c32d
Ruslan Akhmedov <etzheded@gmail.com>
2025-04-19 13:46:10 +0300

    feat: добавлен роутер страницы входа

--- client/src/router/router.ts
+++ client/src/router/router.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/home.vue";
 import Registration from "../views/reg.vue";
+import Login from "../views/login.vue";
 import Chat from "../views/chat.vue";
 
 const routes = [
@@ -13,6 +14,11 @@ const routes = [
     path: "/reg",
     name: "reg",
     component: Registration,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
   },
   {
     path: "/chat",
