5c75cd7e26c8652853ac1bf1633eda4342b7e75d
Ruslan Akhmedov <etzheded@gmail.com>
2025-05-21 15:13:46 +0300

    feat(router): защитил маршруты /chat и /chat/:id авторизацией

--- client/src/router/router.ts
+++ client/src/router/router.ts
@@ -22,14 +22,33 @@ const routes = [
   },
   {
     path: "/chat",
+    name: "chats",
+    component: Chat,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/chat/:id",
     name: "chat",
     component: Chat,
+    meta: { requiresAuth: true },
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL), // Изменено на import.meta.env
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+});
+
+function isAuthenticated() {
+  return !!localStorage.getItem("token");
+}
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !isAuthenticated()) {
+    next({ name: "login" });
+  } else {
+    next();
+  }
 });
 
 export default router;
