8ce46157d609c3481f3958cfbd3e3d4b5e471355
Ruslan Akhmedov <etzheded@gmail.com>
2025-05-26 19:55:32 +0300

    feat: добавлено отображение интро-компонента при отсутствии активного чата
    - Добавлен компонент ChatIntro
    - Подключено хранилище чата (useChatStore)
    - ChatWindow отображается только при наличии активного чата
    - В остальных случаях отображается ChatIntro

---
+++ client/src/components/chatWindow/ChatIntro.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div
+    class="flex flex-col justify-center items-center w-[1100px] h-[910px] border-l-1 border-[#1b1b1b] bg-[#252525] rounded-r-lg">
+    <p
+      class="w-[300px] flex justify-center items-center text-[#ffffff] text-[15px] font-bold bg-[#2e2e2e] rounded-[20px]">
+      Выберите, кому хотели бы написать</p>
+  </div>
+</template>
--- client/src/views/chat.vue
+++ client/src/views/chat.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
+import ChatIntro from "../components/chatWindow/ChatIntro.vue";
 import ChatWindow from "../components/chatWindow/ChatWindow.vue";
 import SideBar from "../components/sideBar/sideBar.vue";
+import { useChatStore } from "../stores/chat";
 
+const chat = useChatStore();
 </script>
+
 
 <template>
   <div class="flex flex-row rounded-[20px] shadow-[0_6px_50px_rgba(255,255,255,0.1)] bg-[#252525]">
     <SideBar />
-    <ChatWindow />
+    <template v-if="chat.activeChatId > 0">
+      <ChatWindow />
+    </template><template v-else>
+      <ChatIntro />
+    </template>
   </div>
 </template>
