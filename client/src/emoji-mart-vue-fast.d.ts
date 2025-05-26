ce089f14b7c0b90969c8cc1378aa559c7d94328d
Ruslan Akhmedov <etzheded@gmail.com>
2025-05-26 19:45:36 +0300

    feat: добавлены emoji-mart-vue-fast и pinia для управления состоянием и emoji-поддержки

---
+++ client/src/emoji-mart-vue-fast.d.ts
@@ -0,0 +1,7 @@
+declare module "emoji-mart-vue-fast" {
+  import { DefineComponent } from "vue";
+  const Picker: DefineComponent<any, any, any>;
+  export { Picker };
+}
+declare module "emoji-mart-vue-fast";
+declare module "emoji-mart-vue-fast/src";
